dc98aa441b6c57732effe89b2bf199d4e3ab7c51
robert-min <robertmin522@gmail.com>
2023-03-05 13:53:52 +0900

    feat: download_launches Dag 작성

---
+++ rocket-launch/dags/download_launches.py
@@ -0,0 +1,58 @@
+import json
+import pathlib
+
+import airflow
+import requests
+import requests.exceptions as requests_exceptions
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+# dag : 모든 워크 플로우의 시작점
+dag = DAG(
+    dag_id="download_launches",
+    start_date=airflow.utils.dates.days_ago(14),
+    schedule_interval=None, # Dag 실행 간격, None은 자동으로 실행하지 않음을 의미
+)
+
+download_launches = BashOperator(
+    task_id="download_launches",
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",
+    dag=dag,
+)
+
+
+def _get_pictures():
+    # Ensure directory exists
+    pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
+
+    # Download all pictures in launches.json
+    with open("/tmp/launches.json") as f:
+        launches = json.load(f)
+        image_urls = [launch["image"] for launch in launches["results"]]
+        for image_url in image_urls:
+            try:
+                response = requests.get(image_url)
+                image_filename = image_url.split("/")[-1]
+                target_file = f"/tmp/images/{image_filename}"
+                with open(target_file, "wb") as f:
+                    f.write(response.content)
+                print(f"Downloaded {image_url} to {target_file}")
+            except requests_exceptions.MissingSchema:
+                print(f"{image_url} appears to be an invalid URL.")
+            except requests_exceptions.ConnectionError:
+                print(f"Could not connect to {image_url}.")
+
+
+get_pictures = PythonOperator(
+    task_id="get_pictures", python_callable=_get_pictures, dag=dag
+)
+
+notify = BashOperator(
+    task_id="notify",
+    bash_command='echo "There are now $(ls /tmp/images/ | wc -l) images."',
+    dag=dag,
+)
+
+# DAG 실행순서 정의
+download_launches >> get_pictures >> notify
